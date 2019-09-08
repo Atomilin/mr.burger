dd7bc31ecb72ed9f6d507e2ebcbbc1b5ed5c5a4a
Atomilin <50828840+Atomilin@users.noreply.github.com>
2019-09-08 14:52:42 +0300

    fix script

--- js/modul.js
+++ js/modul.js
@@ -17,20 +17,37 @@ function sendRequest() {
       //отрицательный запрос
       //let url = 'https://webdev-api.loftschool.com/sendmail/fail'
 
-      var formData = new FormData();
+      let formData = new FormData();
       formData.append('name', form.elements.name.value);
       formData.append('phone', form.elements.phone.value);
       formData.append('comment', form.elements.comment.value);
       formData.append('to', 'mail@mail.com');
 
       const xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = processReqChange;
       xhr.responseType = 'json';
       xhr.open('POST', url);
       xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
       xhr.send(formData);
+      
+      function processReqChange() {   
+        
+        if (xhr.readyState == 4 && xhr.status == 200) {
 
-      xhr.addEventListener('load', () => {
+            // открытие модального окна
+          var modul = document.getElementById("modul");
+          modul.style.display = "flex";
+          document.getElementsByClassName('modul__wrapper')[0].style.position = 'fixed';
+          document.body.style.overflow = 'hidden';
+          //задаем новый тектс заголовку
+          document.getElementById("modul__title").textContent = "Заказ отправлен";
+          document.getElementById("modul__text").textContent = "Ваш заказ принят!";
+          
+        }  
+    }
+      /*xhr.addEventListener('LOAD', () => {
         if (xhr.readyState == 4 && xhr.response.status == 200) {
+          
           // открытие модального окна
           var modul = document.getElementById("modul");
           modul.style.display = "flex";
@@ -40,7 +57,7 @@ function sendRequest() {
           document.getElementById("modul__title").textContent = "Заказ отправлен";
           document.getElementById("modul__text").textContent = "Ваш заказ принят!";
         } 
-      });
+      });*/
     } else {
       //вызоы модального окна с сообщением об ошибке и обязательными полями
       // открытие модального окна
